98435469f692fb696524c82b12af92a4ab846c10
Nejc Ševerkar <nseverkar@gmail.com>
2023-04-14 17:30:35 +0200

    extract public key-pair, refactor

--- contracts/PublicKeyToAddress.sol
+++ contracts/PublicKeyToAddress.sol
@@ -6,53 +6,83 @@ import "./EC.sol";
 
 contract PublicKeyToAddress is EC {
 
-    function publicKeyToEthereumAddress(uint[2] memory publicKey) public pure returns (address) {
-        bytes32 hash = keccak256(abi.encodePacked(publicKey));
-        return address(uint160(uint256(hash)));
-    }
-
-    function publicKeyToAvalancheAddress(
-        uint[2] memory publicKey, string memory prefix, uint[] memory hrp
+    function publicKeyToAvalancheAddressString(
+        bytes calldata publicKey, string memory prefix, uint[]memory hrp
     )
         public pure
         returns (string memory)
     {
-        bytes memory compressedPublicKey = compressPublicKey(publicKey[0], publicKey[1]);
-        bytes32 sha = sha256(abi.encodePacked(compressedPublicKey));
-        bytes20 ripesha = ripemd160(abi.encodePacked(sha));
+        bytes20 avalancheAddress = publicKeyToAvalancheAddress(publicKey);
         uint[] memory rp = new uint[](20);
         for(uint i=0;i<20;i++) {
-            rp[i] = uint(uint8(ripesha[i]));
+            rp[i] = uint(uint8(avalancheAddress[i]));
         }
         bytes memory pre = bytes(prefix);
         return encode(pre, hrp, convert(rp, 8, 5));
     }
 
-    function compressPublicKey(uint256 x, uint256 y) public pure returns (bytes memory) {
-        return BytesLib.concat(publicKeyBytePrefix(y % 2 == 0), abi.encodePacked(bytes32(x)));
-    }
-
-    function decompressPublicKey(
-        bytes memory compressedPublicKey
+    function publicKeyToEthereumAddress(
+        bytes calldata publicKey
     )
         public pure
-        returns (uint[2] memory publicKey)
+        returns (bytes20)
     {
-        require(compressedPublicKey.length == 33, "Invalid compressed public key length");
-        bytes1 prefix = compressedPublicKey[0];
-        uint256 x = uint256(BytesLib.toBytes32(compressedPublicKey, 1));
-        // Tonelli–Shanks algorithm for calculating square root modulo prime of x^3 + 7
-        uint256 y = powmod(mulmod(x, mulmod(x, x, p), p) + 7, (p + 1) / 4, p);
-        publicKey[0] = x;
-        publicKey[1] = (prefix == bytes1(0x02)) ?
-            ((y % 2 == 0) ? y : p - y) :
-            ((y % 2 == 0) ? p - y : y);
-        return publicKey;
+        (uint256 x, uint256 y) = extractPublicKeyPair(publicKey);
+        uint256[2] memory publicKeyPair = [x, y];
+        bytes32 hash = keccak256(abi.encodePacked(publicKeyPair));
+        return bytes20(uint160(uint256(hash)));
     }
 
-    function publicKeyBytePrefix(bool evenY) internal pure returns (bytes memory) {
-        // convert byte1 to bytes
+    function publicKeyToAvalancheAddress(
+        bytes calldata publicKey
+    )
+        public pure
+        returns (bytes20)
+    {
+        bytes memory compressedPublicKey = publicKey;
+        if (publicKey[0] != bytes1(0x04)) {
+            (uint256 x, uint256 y) = extractPublicKeyPair(publicKey);
+            compressedPublicKey = compressPublicKey(x, y);
+        }
+        bytes32 sha = sha256(abi.encodePacked(compressedPublicKey));
+        return ripemd160(abi.encodePacked(sha));
+    }
+
+    function extractPublicKeyPair(
+        bytes calldata encodedPublicKey
+    )
+        internal pure
+        returns (uint256, uint256)
+    {
+        bytes1 prefix = encodedPublicKey[0];
+        if (prefix == bytes1(0x04)) {
+            return (
+                uint256(BytesLib.toBytes32(encodedPublicKey, 1)),
+                uint256(BytesLib.toBytes32(encodedPublicKey, 33)));
+        } else {
+            uint256 x = uint256(BytesLib.toBytes32(encodedPublicKey, 1));
+            // Tonelli–Shanks algorithm for calculating square root modulo prime of x^3 + 7
+            uint256 y = powmod(mulmod(x, mulmod(x, x, p), p) + 7, (p + 1) / 4, p);
+            if (prefix == bytes1(0x02)) {
+                return (x, (y % 2 == 0) ? y : p - y);
+            } else if (prefix == bytes1(0x03)) {
+                return (x, (y % 2 == 0) ? p - y : y);
+            } else {
+                revert("Invalid public key prefix");
+            }
+        }
+    }
+
+    function compressPublicKey(uint256 x, uint256 y) internal pure returns (bytes memory) {
+        return BytesLib.concat(compressedPublicKeyBytePrefix(y % 2 == 0), abi.encodePacked(bytes32(x)));
+    }
+
+    function compressedPublicKeyBytePrefix(bool evenY) internal pure returns (bytes memory) {
         return abi.encodePacked(evenY ? bytes1(0x02) : bytes1(0x03));
+    }
+
+    function uncompressedPublicKeyBytePrefix() internal pure returns (bytes memory) {
+        return abi.encodePacked(bytes1(0x04));
     }
 
     function powmod(uint256 x, uint256 n, uint256 p) private pure returns (uint256) {
--- test/publicKeyToAddress.test.ts
+++ test/publicKeyToAddress.test.ts
@@ -10,52 +10,32 @@ import {
 const PublicKeyToAddress = artifacts.require("PublicKeyToAddress");
 
 describe("Tests for address derivation from public key", async () => {
-  let publicKeyToAddress: PublicKeyToAddressInstance
+  let publicKeyToAddress: PublicKeyToAddressInstance;
 
   beforeEach(async() => {
-    publicKeyToAddress = await PublicKeyToAddress.new()
-  })
+    publicKeyToAddress = await PublicKeyToAddress.new();
+  });
 
   it("Should correctly derive ethereum address from public key", async () => {
     for (let i = 0; i < 100; i++) {
       const privateKey = generatePrivateKey();
-      const ethAddress = privateKeyToEthereumAddress(privateKey)
-      const pubpair = privateKeyToPublicKeypair(privateKey)
-      const resp = await publicKeyToAddress.publicKeyToEthereumAddress(pubpair)
-      expect(resp.toLowerCase()).to.equal(ethAddress)
+      const publicKey = Buffer.from(privateKeyToKeypair(privateKey).getPublic(true, 'hex'), 'hex');
+      const resp = await publicKeyToAddress.publicKeyToEthereumAddress(publicKey);
+      expect(resp.toLowerCase()).to.equal(privateKeyToEthereumAddress(privateKey));
     }
-  })
-
-  it("should correctly derive avalanche address from public key", async () => {
-    let prefix = "fuji"
-    let hrp: Array<number> = [] //array of unicode of prefix
-    for (var i=0; i<prefix.length; i++) {
-      hrp[i] = prefix.charCodeAt(i)
-    }
-    for (let i = 0; i < 100; i++) {
-      const privateKey = generatePrivateKey()
-      const avaAddress = privateKeyToAvalancheAddress(privateKey, "fuji")
-      const pubpair = privateKeyToPublicKeypair(privateKey)
-      const resp = await publicKeyToAddress.publicKeyToAvalancheAddress(pubpair, prefix, hrp)
-      expect(resp).to.equal(avaAddress)
-    }
-  })
-
-  it("should test public key decompression", async () => {
-    const privateKey = "8944ff161a80575ff14b2216fd7c19edc085f2d4a3c00a923ffc77ce64d6ffa7"
-    const pubpair = privateKeyToPublicKeypair(privateKey)
-    const compressedPublicKey = privateKeyToKeypair(privateKey).getPublic(true, 'hex').padStart(66, '0')
-    const decompressedPublicKey = await publicKeyToAddress.decompressPublicKey(Buffer.from(compressedPublicKey, 'hex'))
-    expect(decompressedPublicKey[0].toString()).to.equal(pubpair[0])
-    expect(decompressedPublicKey[1].toString()).to.equal(pubpair[1])
   });
 
-  it("should test the public key compression", async () => {
-    const privateKey = "8944ff161a80575ff14b2216fd7c19edc085f2d4a3c00a923ffc77ce64d6ffa7"
-    const pubpair = privateKeyToPublicKeypair(privateKey)
-    const keypair =privateKeyToKeypair(privateKey)
-    const publicKey = Buffer.from(keypair.getPublic(true, 'hex').padStart(66, '0'), 'hex')
-    const resp = await publicKeyToAddress.compressPublicKey(pubpair[0], pubpair[1])
-    expect(resp).to.equal(`0x${publicKey.toString('hex')}`)
-  })
-})
+  it("should correctly derive avalanche address from public key", async () => {
+    let prefix = "fuji";
+    let hrp: Array<number> = []; //array of unicode of prefix
+    for (var i=0; i<prefix.length; i++) {
+      hrp[i] = prefix.charCodeAt(i);
+    }
+    for (let i = 0; i < 100; i++) {
+      const privateKey = generatePrivateKey();
+      const publicKey = Buffer.from(privateKeyToKeypair(privateKey).getPublic(true, 'hex'), 'hex');
+      const resp = await publicKeyToAddress.publicKeyToAvalancheAddressString(publicKey, prefix, hrp);
+      expect(resp).to.equal(privateKeyToAvalancheAddress(privateKey, "fuji"));
+    }
+  });
+});
